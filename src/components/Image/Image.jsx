d9d9142aa26530d0278573f5ae3fdbe7f0214f18
dmitry.bobrov <abiatop@gmail.com>
2021-03-18 09:04:53 +0300

    image2

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,25 +1,30 @@
 import React, {useState, useEffect, useRef} from "react";
 import {Image as KonvaImage} from "react-konva";
+import useImage from "use-image";
+const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari']);
 
-const Image = React.forwardRef((props, ref) => {
 
-    const [image, setImage] = useState(new window.Image());
+    const Image = React.forwardRef((props, ref) => {
+        const [image, setImage] = useState(new window.Image());
+        const [image, status] = useImage(props.src, 'Anonymous');
+        const localRef = useRef(null);
+        const KonvaImageRef = ref || localRef;
 
-    const localRef = useRef(null);
-    const KonvaImageRef = ref || localRef;
+        useEffect(() => {
+            const img = new window.Image();
+            img.src = props.src;
+            img.crossOrigin = 'Anonymous';
+            img.onload = () => {
+                setImage(img);
+            };
+        }, [props.src, props]);
 
-    useEffect(() => {
-        const img = new window.Image();
-        img.src = props.src;
-        img.crossOrigin = 'Anonymous';
-        img.onload = () => {
-            setImage(img);
-        };
-    }, [props.src, props]);
+        return (
+            <React.Fragment>
+                <KonvaImage {...props} image={image} ref={KonvaImageRef}/>
+            </React.Fragment>
+        )
 
-    return (
-        <KonvaImage {...props} image={image} ref={KonvaImageRef}/>
-    );
+    });
 });
-
 export default Image;
