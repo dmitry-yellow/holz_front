75ee378628dad0e44d3d710e4000c54720bae980
fitkovich1 <fitkovich1@gmail.com>
2021-03-18 14:33:39 +0300

    corrected Image.jsx file for macOS

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,36 +1,31 @@
-import React, {useState, useEffect, useRef} from "react";
-import {Image as KonvaImage} from "react-konva";
+import React, { useState, useEffect, useRef } from "react";
+import { Image as KonvaImage } from "react-konva";
 import useImage from "use-image";
-const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari']);
+
+const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) {
+  return p.toString() === "[object SafariRemoteNotification]";
+})(!window['safari']);
 
 
-    const Image = React.forwardRef((props, ref) => {
+const Image = React.forwardRef((props, ref) => {
 
-        const [image, status] = useImage(props.src, 'Anonymous');
-        const [imageState, setImage] = useState(image);
-        const localRef = useRef(null);
-        const KonvaImageRef = ref || localRef;
+  const [image, status] = useImage(props.src, 'Anonymous');
+  const [imageState, setStateImage] = useState(image);
+  const localRef = useRef(null);
+  const KonvaImageRef = ref || localRef;
 
-        useEffect(() => {
-            if(status=='loaded') {
-                    setImage(image);
-            }
-        }, [props.src, props]);
+  useEffect(() => {
+    if (status == 'loaded') {
+      setStateImage(image);
+    }
+  }, [props.src, props, image]);
 
 
-    /*    if(isSafari) {*/
-            return (
-
-                <React.Fragment>
-                    <KonvaImage {...props} image={imageState} ref={KonvaImageRef}/>
-                </React.Fragment>
-
-            )
-        /*}else{
-            return (<KonvaImage {...props} image={imageState} ref={KonvaImageRef}/>
-            )
-        }*/
-
-    });
+  return (
+      <React.Fragment>
+        <KonvaImage { ...props } image={ imageState } ref={ KonvaImageRef }/>
+      </React.Fragment>
+  )
+});
 
 export default Image;
