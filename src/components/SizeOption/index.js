f95765a95b856c7c680db3231b9150477750bd5f
fitkovich1 <fitkovich1@gmail.com>
2021-08-04 14:16:30 +0300

    corrected texts in size option

--- src/components/SizeOption/index.js
+++ src/components/SizeOption/index.js
@@ -1,21 +1,22 @@
 import cn from "classnames";
 import './style.css';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 
 const SizeOption = ( props ) => {
 
   const dispatch = useDispatch();
+  const selectedTypeId = useSelector(state => state.hotTub.selectedTypeId);
   const { sizeData, selectedSizeId, setSelectedSizeId } = props;
 
   return (
       <div className="SizeOption">
-        {/*<p>Größe</p>*/}
         { sizeData ? Object.values(sizeData).map((size, index) => {
 
           const price = size.base.price;  // object with prices
           const main = size["_main"];     // name and id info
           const countPeople = ['4-6', '6-8']; //Свойство добавить на сколько персон в админку
+
 
           return <div key={ main.id }
                       onClick={ () => dispatch(setSelectedSizeId(main.id)) }
@@ -24,8 +25,9 @@ const SizeOption = ( props ) => {
               <p className="SizeOption-item-name">{ size['_main'].Name }</p>
               <p className="SizeOption-item-price">{ price.realValue } { price.currency.currencySymbol }</p>
             </div>
-            <p>{ countPeople[index] } personen</p>
-            <p>Durchmesser - { main.Name.trim().slice(0, 3) } cm</p>
+            <p>{ selectedTypeId !== 80690 ? countPeople[index] : '2' } Personen</p>
+            <p>{ selectedTypeId !== 80690 ? 'Durchmesser' : 'Länge'} - { main.Name.trim().slice(0, 3) } cm</p>
+
           </div>
         }) : null }
       </div>
