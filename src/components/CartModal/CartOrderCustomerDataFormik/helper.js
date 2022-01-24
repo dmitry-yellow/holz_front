ccf329d735cff754890b48f8182e969d041b018b
marina-bovkunovich <marina.b@abiatec.com>
2022-01-24 19:19:57 +0300

    create default data for selectors

---
+++ src/components/CartModal/CartOrderCustomerDataFormik/helper.js
@@ -0,0 +1,17 @@
+export const countriesArray = [
+  { name: "Afghanistan" },
+  { name: "Bangladesch" },
+  { name: "Britische Jungferninseln" },
+  { name: "Ghana" },
+  { name: "Japan" },
+  { name: "Liberia" },
+  { name: "Deutschland" },
+  { name: "Afghanistan2" },
+  { name: "Bangladesch2" },
+  { name: "Britische Jungferninseln2" },
+  { name: "Ghana2" },
+  { name: "Japan2" },
+  { name: "Liberia2" },
+];
+
+export const countries = countriesArray.map(value => value.name)
